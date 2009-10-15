e2face6cde21e9ce3926ea374c57be3e9fc016a0
ckreutzer <herr.kreutzer@gmail.com>
2009-10-15 21:34:36 +0200

    wrapped threading with coroutines to avoid problems with django's db access

--- webchat/run.py
+++ webchat/run.py
@@ -11,18 +11,19 @@ from django.core.signals import got_request_exception
 sys.path.append('..')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'webchat.settings'
 
+def exception_printer(sender, **kwargs):
+    traceback.print_exc()
 
+got_request_exception.connect(exception_printer)
 
 call_command('syncdb')
 application = WSGIHandler()
 
 if __name__ == '__main__':
-    from eventlet import api, wsgi
+    from eventlet import api, wsgi, util
     
-    def exception_printer(sender, **kwargs):
-        traceback.print_exc()
-
-    got_request_exception.connect(exception_printer)
-    
+    # wrapping threading with coros to prevent problems with django's db access
+    util.wrap_threading_local_with_coro_local()
+        
     print 'Serving on 8080...'
     wsgi.server(api.tcp_listener(('', 8080)), application)
